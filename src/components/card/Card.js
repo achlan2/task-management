ed19b9194dafd3fa9fc2d1fab3a3edeee92f8ab0
achlanbima <achlan_00011101@hotmail.com>
2021-01-16 06:21:38 +0700

    rename card folder

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -2,6 +2,14 @@ import React from 'react'
 import './Card.css'
 
 const Card = ({title, weight}) => {
+
+  const [isOpen, setOpen] = useState(false)
+
+  const dropdownToggle = (e)=>{
+    e.preventDefault()
+    setOpen(isOpen=>!isOpen)
+  }
+
   return (
     <div className='card'>
       <h3 className="title">{title}</h3>
@@ -10,7 +18,7 @@ const Card = ({title, weight}) => {
           <img src="/icons/weight.svg" alt="weight-icon"/>
           <p>{weight}%</p>
         </div>
-        <button>
+        <button onClick={dropdownToggle}>
           <img src="/icons/dots.svg" alt="dot"/>
         </button>
       </div>
