059cf4171128a632fe4dc76135ab8c3ea9e4bfcb
achlanbima <achlan_00011101@hotmail.com>
2021-01-16 20:04:05 +0700

    add addTask reducer

--- src/context/TaskContext.js
+++ src/context/TaskContext.js
@@ -2,24 +2,36 @@ import createDataContext from './createDataContext'
 import happyApi from '../api/happy'
 
 const taskReducer = (state, action) => {
-  switch(action.type){
-    case 'remove_task':{
-      const {from, id} = action.payload
-      const filter = Array.from(state[from]).filter(task=>task.id !== id)
+  switch (action.type) {
+    case 'remove_task': {
+      const { from, id } = action.payload
+      const filter = Array.from(state[from]).filter(task => task.id !== id)
       return {
         ...state,
         [from]: filter
       }
     }
-    case 'move_task':{
+
+    case 'add_task': {
+      const { to, data } = action.payload
+      let target = Array.from(state[to])
+      console.log('data reducer', data, target)
+      target.push(data)
+      return {
+        ...state,
+        [to]: target
+      }
+    }
+
+    case 'move_task': {
       const { from, to, data } = action.payload
-      const removed = Array.from(state[from]).filter(task=>task.id !== data.id) 
+      const removed = Array.from(state[from]).filter(task => task.id !== data.id)
       let target = Array.from(state[to])
       target.push(data)
       return {
         ...state,
-        [from] : removed,
-        [to] : target
+        [from]: removed,
+        [to]: target
       }
     }
     case 'fetch_task': {
@@ -35,29 +47,25 @@ const taskReducer = (state, action) => {
 }
 
 const fetchTaskPerBoard = dispatch => async (id) => {
-  // dispatch({
-  //   type:'init_task',
-  //   payload: id
-  // })
   try {
     const response = await happyApi.get(`/boards/${id}/tasks`);
-    dispatch({ 
-      type:'fetch_task',  
+    dispatch({
+      type: 'fetch_task',
       payload: {
         data: response.data,
         boardId: id
       }
     })
-    
+
   } catch (error) {
-    console.log('err',error)
+    console.log('err', error)
   }
 }
 
 const removeTask = dispatch => (from, id) => {
   dispatch({
-    type:'remove_task',
-    payload:{
+    type: 'remove_task',
+    payload: {
       from,
       id
     }
@@ -67,24 +75,54 @@ const removeTask = dispatch => (from, id) => {
 const moveTask = dispatch => (from, to, data) => {
   dispatch({
     type: 'move_task',
-    payload:{
+    payload: {
       from,
       to,
-      data:{
-        id:data.id,
-        title:data.title,
-        userId:data.userId,
-        weight:data.weight,
-        createdAt:data.createdAt,
-        updatedAt:data.updatedAt,
-        boardId:to
+      data: {
+        id: data.id,
+        title: data.title,
+        userId: data.userId,
+        weight: data.weight,
+        createdAt: data.createdAt,
+        updatedAt: data.updatedAt,
+        boardId: to
       }
     }
   })
 }
 
-export const {Provider, Context} = createDataContext(
+const addTask = dispatch => async (boardId, title, weight) => {
+
+  try {
+
+    const numValidate = /^\d+$/
+    let data = {}
+    if (numValidate.test(weight)) {
+      data = {
+        title, weight
+      }
+    } else {
+      data = { title }
+    }
+
+    const response = await happyApi.post(`/boards/${boardId}/tasks`, data)
+
+    console.log('response', response.data)
+    dispatch({
+      type: 'add_task',
+      payload: {
+        to: boardId,
+        data: response.data
+      }
+    })
+
+  } catch (e) {
+
+  }
+}
+
+export const { Provider, Context } = createDataContext(
   taskReducer,
-  { fetchTaskPerBoard, removeTask, moveTask },
+  { fetchTaskPerBoard, removeTask, moveTask, addTask },
   []
 )
