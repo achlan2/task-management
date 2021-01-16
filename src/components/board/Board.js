b9e20f3f980ad2d1bd88ea9b59fd2a4dd48df327
achlanbima <achlan_00011101@hotmail.com>
2021-01-16 23:08:08 +0700

    move no-task class to ouside of card-container

--- src/components/board/Board.js
+++ src/components/board/Board.js
@@ -19,25 +19,27 @@ const Board = ({ title, description, id, index, boardLength, nextId, prevId }) =
     <div className='board' key={id}>
       <h4>{title}</h4>
       <h4 className="description">{description}</h4>
-      <div className="card-container">
-        {listReady && state[id].length !== 0 ?
-          state[id].map((card) => (
-            <Card
-              key={card.id}
-              boardId={id}
-              title={card.title}
-              weight={card.weight}
-              id={card.id}
-              position={index === 0 ? 'first' : index === boardLength - 1 ? 'end' : 'center'}
-              moveLeft={() => moveTask(id, prevId, card)}
-              moveRight={() => moveTask(id, nextId, card)}
-              removeTask={() => removeTask(id, card.id)}
-            />
-          ))
-          : (
-            <p className='no-task'>No Task Available</p>
-          )}
-      </div>
+      {listReady && state[id].length !== 0 ?
+        (
+          <div className="card-container">
+            {state[id].map((card) => (
+              <Card
+                key={card.id}
+                boardId={id}
+                title={card.title}
+                weight={card.weight}
+                id={card.id}
+                position={index === 0 ? 'first' : index === boardLength - 1 ? 'end' : 'center'}
+                moveLeft={() => moveTask(id, prevId, card)}
+                moveRight={() => moveTask(id, nextId, card)}
+                removeTask={() => removeTask(id, card.id)}
+              />
+            ))
+            }
+          </div>
+        ) : (
+          <p className='no-task'>No Task Available</p>
+        )}
       <div className="add-wrapper" onClick={() => setCreateOpen(true)}>
         <div className="img-wrapper">
           <img src="/icons/plus.svg" alt="plus-icon" />
