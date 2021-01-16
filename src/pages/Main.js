de8188c08ad3841b83c47e02700cf88aa021c603
achlanbima <achlan_00011101@hotmail.com>
2021-01-16 11:14:03 +0700

    use data from api

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,19 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Board from '../components/board/Board'
+import happyApi from '../api/happy'
 
 const Main = () => {
+
+  const [boards, setBoards] = useState([])
+
+  useEffect(() => {
+    const fetch = async()=>{
+      const response = await happyApi.get('/boards');
+      setBoards(response.data)
+    }
+
+    fetch()
+  }, [])
+
   return (
     <div className='main-container'>
-      <Board title='Q1 2019' subtitle='January - March' />
-      <Board title='Q1 2019' subtitle='January - March' />
-      <Board title='Q1 2019' subtitle='January - March' />
-      <Board title='Q1 2019' subtitle='January - March' />
-      <Board title='Q1 2019' subtitle='January - March' />
-      <Board title='Q1 2019' subtitle='January - March' />
-      <Board title='Q1 2019' subtitle='January - March' />
-      <Board title='Q1 2019' subtitle='January - March' />
-      <Board title='Q1 2019' subtitle='January - March' />
-      <Board title='Q1 2019' subtitle='January - March' />
+      {boards.length !== 0 ? 
+        boards.map(board=>(
+
+          <Board title={board.title} subtitle={board.description} key={board.id}/>
+        ))
+      :null}
     </div>
   )
 }
