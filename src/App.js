73f8840d055d9ae046d8ee8011c9e41339b2f9f8
achlanbima <achlan_00011101@hotmail.com>
2021-01-16 20:01:42 +0700

    add alt to logo

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import React from 'react'
 import "./App.css";
 import Main from './pages/Main';
-import {Provider as BoardProvider} from './context/BoardContext'
-import {Provider as TaskProvider} from './context/TaskContext'
+import { Provider as BoardProvider } from './context/BoardContext'
+import { Provider as TaskProvider } from './context/TaskContext'
 
 const App = () => {
   return (
     <div className='container'>
       <div className="sidebar">
-        <img src='/logo.svg'/>
+        <img src='/logo.svg' alt='logo' />
       </div>
       <div className="main-section">
         <h1>Product Roadmap</h1>
         <BoardProvider>
           <TaskProvider>
-            <Main/>
+            <Main />
           </TaskProvider>
         </BoardProvider>
       </div>
