049da54af710fe582e0e7eb137948b19eabd67ed
achlanbima <achlan_00011101@hotmail.com>
2021-01-16 08:17:44 +0700

    add create task button

--- src/components/board/Board.js
+++ src/components/board/Board.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import Card from '../card/Card'
 
 const Board = ({title, subtitle}) => {
   return (
     <div className='board'>
       <h4>{title}</h4>
       <h4 className="subtitle">{subtitle}</h4>
+      <div className="card-container">
+        <Card title='Re-designed the zero-g doggie bags. No more spills!' weight='64' />
+        <Card title='Re-designed the zero-g doggie bags. No more spills!' weight='64' />
+      </div>
+      <div className="add-wrapper">
+        <div className="img-wrapper">
+          <img src="/icons/plus.svg" alt="plus-icon"/>
+        </div>
+        <p>Create new task</p>
+      </div>
     </div>
   )
 }
