d36d380c67778ae86bedab9bcde99eaf0f1705ca
achlanbima <achlan_00011101@hotmail.com>
2021-01-16 07:45:56 +0700

    add dropdown wrapper

--- src/components/dropdown/Dropdown.js
+++ src/components/dropdown/Dropdown.js
@@ -1,32 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Modal from '../../layout/Modal'
 import Overlay from '../../layout/Overlay'
+import DeleteDialog from '../dialog/DeleteDialog'
 import './Dropdown.css'
 
 const Dropdown = ({onClose, position}) => {
+  const [isDeleteOpen, setDeleteOpen] = useState(false)
+  const [isEditOpen, setEditOpen] = useState(false)
+
   return (
     <>
       <Overlay callback={onClose}/>
       <ul className="dropdown">
         {position === 'end' || position == 'center' ? (
           <li>
-            <img src="/icons/left-arrow.svg" alt="left-icon"/>
+            <div className="img-wrapper">
+              <img src="/icons/left-arrow.svg" alt="left-icon"/>
+            </div>
             <p>Move Left</p>
           </li>
         ) : position === 'first' || position == 'center' ? (
           <li>
-            <img src="/icons/right-arrow.svg" alt="right-icon"/>
+            <div className="img-wrapper">
+              <img src="/icons/right-arrow.svg" alt="right-icon"/>
+            </div>
             <p>Move Right</p>
           </li>
         ):null}
-        <li>
-          <img src="/icons/pencil.svg" alt="pencil-icon"/>
+        <li onClick={()=>setEditOpen(true)}>
+          <div className="img-wrapper">
+            <img src="/icons/pencil.svg" alt="pencil-icon"/>
+          </div>
           <p>Edit</p>
         </li>
-        <li>
-          <img src="/icons/drop.svg" alt="drop-icon"/>
+        <li onClick={()=>setDeleteOpen(true)}>
+          <div className="img-wrapper">
+            <img src="/icons/drop.svg" alt="drop-icon"/>
+          </div>
           <p>Delete</p>
         </li>
       </ul>
+      <Modal open={isDeleteOpen} onClose={()=>setDeleteOpen(false)}>
+        <DeleteDialog/>
+      </Modal>
     </>
   )
 }
