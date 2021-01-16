f086044515f61df8f4e5ab6c19380b5cf846b9e4
achlanbima <achlan_00011101@hotmail.com>
2021-01-16 20:05:30 +0700

    pass boardId to add form

--- src/components/board/Board.js
+++ src/components/board/Board.js
@@ -2,18 +2,17 @@ import React, { useContext, useEffect, useState } from 'react'
 import Modal from '../../layout/Modal'
 import Card from '../card/Card'
 import ManageTaskDialog from '../dialog/ManageTaskDialog'
-import happyApi from '../../api/happy'
-import {Context as TaskContext} from '../../context/TaskContext'
+import { Context as TaskContext } from '../../context/TaskContext'
 
-const Board = ({title, description,id, index, boardLength, nextId, prevId}) => {
+const Board = ({ title, description, id, index, boardLength, nextId, prevId }) => {
 
   const [listReady, setListReady] = useState(false)
 
   const [isCreateOpen, setCreateOpen] = useState(false)
-  const {fetchTaskPerBoard, removeTask, moveTask, state} = useContext(TaskContext)
+  const { fetchTaskPerBoard, removeTask, moveTask, state } = useContext(TaskContext)
 
   useEffect(() => {
-    fetchTaskPerBoard(id).then(()=>setListReady(true))
+    fetchTaskPerBoard(id).then(() => setListReady(true))
   }, [])
 
   return (
@@ -22,30 +21,31 @@ const Board = ({title, description,id, index, boardLength, nextId, prevId}) => {
       <h4 className="description">{description}</h4>
       <div className="card-container">
         {listReady && state[id].length !== 0 ?
-          state[id].map((card)=>(
-            <Card 
-              key={card.id} 
-              title={card.title} 
-              weight={card.weight} 
-              id={card.id} 
-              position={index === 0 ? 'first' : index === boardLength-1 ? 'end' : 'center'} 
-              moveLeft={()=>moveTask(card.boardId, prevId,card)} 
-              moveRight={()=>moveTask(card.boardId, nextId,card)} 
-              removeTask={()=>removeTask(card.boardId, card.id)}
+          state[id].map((card) => (
+            <Card
+              key={card.id}
+              boardId={id}
+              title={card.title}
+              weight={card.weight}
+              id={card.id}
+              position={index === 0 ? 'first' : index === boardLength - 1 ? 'end' : 'center'}
+              moveLeft={() => moveTask(id, prevId, card)}
+              moveRight={() => moveTask(id, nextId, card)}
+              removeTask={() => removeTask(id, card.id)}
             />
           ))
-        :(
-          <p className='no-task'>No Task Available</p>
-        )}
+          : (
+            <p className='no-task'>No Task Available</p>
+          )}
       </div>
-      <div className="add-wrapper" onClick={()=>setCreateOpen(true)}>
+      <div className="add-wrapper" onClick={() => setCreateOpen(true)}>
         <div className="img-wrapper">
-          <img src="/icons/plus.svg" alt="plus-icon"/>
+          <img src="/icons/plus.svg" alt="plus-icon" />
         </div>
         <p>Create new task</p>
       </div>
-      <Modal open={isCreateOpen} onClose={()=>setCreateOpen(false)}>
-        <ManageTaskDialog onClose={()=>setCreateOpen(false)} type='create'/>
+      <Modal open={isCreateOpen} onClose={() => setCreateOpen(false)}>
+        <ManageTaskDialog onClose={() => setCreateOpen(false)} type='create' boardId={id} />
       </Modal>
     </div>
   )
