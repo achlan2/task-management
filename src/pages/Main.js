bcdf2d3cfca22c6a4ee3fa93abc3b7a490e108a9
achlanbima <achlan_00011101@hotmail.com>
2021-01-16 20:32:56 +0700

    remove comment

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -26,18 +26,9 @@ const Main = () => {
     <div className='main-container'>
       {state.length !== 0 ?
         state.map((board, index) => (
-
           <Board title={board.title} description={board.description} id={board.id} key={board.id} index={index} boardLength={state.length} prevId={index === 0 ? null : state[index - 1].id} nextId={index === state.length - 1 ? null : state[index + 1].id} moveTask={moveTask} />
         ))
         : null}
-      {/* {boards.length !== 0 ? 
-        boards.map((board, index)=>(
-
-          <Board title={board.title} description={board.description} id={board.id} key={board.id} index={index} boardLength={boards.length}/>
-          ))
-          :null} */}
-      {/* <Board title={boards[0].title} description={boards[0].description} id={boards[0].id} key={boards[0].id} index={0} boardLength={boards.length}/>
-          <Board title={boards[2].title} description={boards[2].description} id={boards[2].id} key={boards[2].id} index={0} boardLength={boards.length}/> */}
     </div>
   )
 }
