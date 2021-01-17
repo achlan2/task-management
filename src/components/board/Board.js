54be6bec16e343e31867af3836b4020a19906828
achlanbima <achlan_00011101@hotmail.com>
2021-01-17 12:24:16 +0700

    setup dnd

--- src/components/board/Board.js
+++ src/components/board/Board.js
@@ -3,6 +3,7 @@ import Modal from '../../layout/Modal'
 import Card from '../card/Card'
 import ManageTaskDialog from '../dialog/ManageTaskDialog'
 import { Context as TaskContext } from '../../context/TaskContext'
+import { Draggable, Droppable } from 'react-beautiful-dnd'
 
 const Board = ({ title, description, id, indexBoard, boardLength, nextId, prevId }) => {
 
@@ -15,34 +16,42 @@ const Board = ({ title, description, id, indexBoard, boardLength, nextId, prevId
     fetchTaskPerBoard(id).then(() => setListReady(true))
   }, [])
 
-  // console.log('board data', id, state[id])
+  console.log('board data', id, state[id])
 
   return (
     <div className='board' key={id}>
       <h4>{title}</h4>
       <h4 className="description">{description}</h4>
-      {listReady && state[id].length !== 0 ?
-        (
-          <div className="card-container">
-            {state[id].map((card, index) => (
-              <Card
-                key={card.id}
-                boardId={id}
-                title={card.title}
-                weight={card.weight}
-                id={card.id}
-                position={indexBoard === 0 ? 'first' : indexBoard === boardLength - 1 ? 'end' : 'center'}
-                moveLeft={() => moveTask(id, prevId, card)}
-                moveRight={() => moveTask(id, nextId, card)}
-                removeTask={() => removeTask(id, card.id)}
-                cardIndex={index}
-              />
-            ))
+      <Droppable droppableId={`${id}`}>
+        {(provided, snapshot) => (
+          <div
+            className="card-container"
+            ref={provided.innerRef}
+            {...provided.droppableProps}
+          >
+            {
+              listReady && state[id].length !== 0 ?
+                state[id].map((card, index) => (
+                  <Card
+                    key={card.id}
+                    boardId={id}
+                    title={card.title}
+                    weight={card.weight}
+                    id={card.id}
+                    position={indexBoard === 0 ? 'first' : indexBoard === boardLength - 1 ? 'end' : 'center'}
+                    moveLeft={() => moveTask(id, prevId, card)}
+                    moveRight={() => moveTask(id, nextId, card)}
+                    removeTask={() => removeTask(id, card.id)}
+                    cardIndex={index}
+                  />
+                ))
+                : (
+                  <p className='no-task'>No Task Available</p>
+                )
             }
+            {provided.placeholder}
           </div>
-        ) : (
-          <p className='no-task'>No Task Available</p>
-        )}
+        )}</Droppable>
       <div className="add-wrapper" onClick={() => setCreateOpen(true)}>
         <div className="img-wrapper">
           <img src="/icons/plus.svg" alt="plus-icon" />
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,4 +1,5 @@
 import React, { useContext, useEffect } from 'react'
+import { DragDropContext } from 'react-beautiful-dnd'
 import Board from '../components/board/Board'
 import { Context as BoardContext } from '../context/BoardContext'
 
@@ -11,22 +12,28 @@ const Main = () => {
   }, [])
 
   return (
-    <div className='main-container'>
-      {state.length !== 0 ?
-        state.map((board, index) => (
-          <Board
-            title={board.title}
-            description={board.description}
-            id={board.id}
-            key={board.id}
-            indexBoard={index}
-            boardLength={state.length}
-            prevId={index === 0 ? null : state[index - 1].id}
-            nextId={index === state.length - 1 ? null : state[index + 1].id}
-          />
-        ))
-        : null}
-    </div>
+    <DragDropContext onDragEnd={(result) => {
+      console.log('result.source')
+    }}
+      onDragStart={() => console.log('drag')}
+    >
+      <div className='main-container'>
+        {state.length !== 0 ?
+          state.map((board, index) => (
+            <Board
+              title={board.title}
+              description={board.description}
+              id={board.id}
+              key={board.id}
+              indexBoard={index}
+              boardLength={state.length}
+              prevId={index === 0 ? null : state[index - 1].id}
+              nextId={index === state.length - 1 ? null : state[index + 1].id}
+            />
+          ))
+          : null}
+      </div>
+    </DragDropContext>
   )
 }
 
